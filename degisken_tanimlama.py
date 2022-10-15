40edd0d16c1ee67a0163ba677b4f8c26024d77dc
MehmetAsim <114797924+MehmetAsim@users.noreply.github.com>
2022-10-15 09:55:00 +0300

    asdafds
    afdsafds

--- degisken_tanimlama.py
+++ degisken_tanimlama.py
@@ -1,5 +1,48 @@
-#Değişkenler olmadan 2 öğrencinin sınav notunu hesaplayalım
-print( (80*0.40) + (75*0.60)  )
-print( (45*0.40) + (60*0.60)  )
+#Değişkenler olmadan 2 öğrencinin sınav notunu hesaplayalım.
+# print( (80*0.40) + (75*0.60)  )
+ # print( (45*0.40) + (60*0.60)  )
+ #değişkenlerle 2 öğrencinin sınav notlarını hesaplayalım.
+sinav1_yuzde = 0.4
+sinav2_yuzde = 0.6
+
+print(   (80*sinav1_yuzde) + (75*sinav2_yuzde)   )
+print(   (45*sinav1_yuzde) + (60*sinav2_yuzde)   )
+
+# DEĞİŞKEN TANIMLAMA KURALLARI
 
 
+# Rakam ile başlayamaz
+# 1sayi = 85 hata verdi
+sayi1 = 85
+print (sayi1)
+
+#Büyük küçük harfe duyarlıdır
+number = 12
+NUMBER = 15
+print (number)
+print (NUMBER)
+
+#Türkçe karakterler kullanılmaz
+# yaş  = 18 hata verdi
+
+yas = 18
+
+age = 17
+
+print(yas)
+print(age)
+
+x = 1 #integer
+
+y = 1.2 # float
+
+ad = "Asim" #string
+
+sinav_basarili_mi = True #boolean
+
+print(x+y)
+
+print(x+ad) #hata verdi
+
+print(x+sinav_basarili_mi)
+print(ad+sinav_basarili_mi) # hata verdi
