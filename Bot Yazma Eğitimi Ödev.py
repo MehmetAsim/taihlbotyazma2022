47472e5822d2954d4f60f30d5b7f672bb2ab264a
MehmetAsim <114797924+MehmetAsim@users.noreply.github.com>
2022-10-20 22:00:30 +0300

    Create Bot Yazma Eğitimi Ödev.py
    ödev

---
+++ Bot Yazma Eğitimi Ödev.py
@@ -0,0 +1,28 @@
+
+url = "https://teknolojiaihl.meb.k12.tr"
+
+# 1- "url" içinde kaç karakter olduğunu yazdır.
+print(len(url))
+
+# 2- "url" içindeki "meb" sözcüğünü ekrana yazdırın.
+print(url[22])
+print(url[23])
+print(url[24])
+
+# 3- "url" içindeki "k12" sözcüğünü ekrana yazdırın.
+print(url[26])
+print(url[27])
+print(url[28])
+
+# 4- Kullanıcıdan adını, en sevdiği yemeği alın ve cümle içinde yazdırın.
+# ÖRNEK CÜMLE: Adınız: Eren. En sevdiğiniz yemek: Pırasa
+
+name = input("adın ne:  ")
+
+# 5- Öğrencinin 2 sınav notunu alın. Notunu hesaplayıp cümle içinde yazdırın.
+x =input("1. Sınav Notu")
+z =input("2. Sınav Notu")
+print((float(x))*0.35+(float(z)*0.65))
+
+
+
