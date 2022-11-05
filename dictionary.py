2f6abc371fcf4987b177d4773819f50d5fe78f2c
MehmetAsim <mehmetasimkus@gmail.com>
2022-11-05 11:59:31 +0300

    asdfasdfasdf
    asdfasdf

--- dictionary.py
+++ dictionary.py
@@ -8,6 +8,27 @@ numaralar = [66 , 75]
 
 numara  = int(input("Öğrenci Numarasını Yazınız: "))
 
-print(numaralar.index(numara))
+#print(numaralar.index(numara))
 
+ogrenciler = {66:"Ahmet" ,75:"Mehmet" }
+print(ogrenciler[numara])
+
+kisiler = {
+
+    1: {
+        "ad": "Mehmet",
+        "soyad":"Kuş",
+        "cinsiyet": True,
+        "dersler": ["Matematik" , "Fizik" , "Kimya"]
+       },
+    45: {
+        "ad":"Zeynep",
+        "soyad":"Kuş",
+        "cinsiyet": False,
+        "dersler": ["Matematik" , "Fen Bilimleri" , "Beden Eğitimi"]
+
+          }
+}
+
+print(kisiler[45]["dersler"])
 
