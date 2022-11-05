ba5b71817d37466fb7cb82262f5746c1f1dd27b9
MehmetAsim <mehmetasimkus@gmail.com>
2022-11-05 11:43:34 +0300

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+"""
+Dictionary "anahtar", "değer" ikililerinden oluşur
+    "ad": "Mehmet Asım"
+    "soyad": "KUŞ"
+"""
+isimler = ("Ahmet" , "Mehmet")
+numaralar = [66 , 75]
+
+numara  = int(input("Öğrenci Numarasını Yazınız: "))
+
+print(numaralar.index(numara))
+
+
