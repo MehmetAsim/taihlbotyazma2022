eea96cf637a5bae9ab65928c3a5f58cf2047060b
MehmetAsim <mehmetasimkus@gmail.com>
2022-11-05 11:26:39 +0300

    lkıjnğo
    jhb

---
+++ nasddas.py
@@ -0,0 +1,43 @@
+
+sayiler = [9,12,85,3,16,34,42,99,165,15]
+
+harfler = ["t","c","a","e","z","m","h","g"]
+
+print(len(sayiler))
+
+print(len(harfler))
+
+print(min(harfler))
+
+print(min(sayiler))
+
+
+print(max(sayiler))
+
+print(max(harfler))
+birlesmis = sayiler+harfler
+
+# print(max(birlesmis)
+
+sayiler.append(95)
+
+print(sayiler)
+
+harfler.insert(3,"b")
+
+
+print(harfler)
+
+sayiler.pop(3)
+
+print(sayiler)
+
+harfler.pop(3)
+
+print(harfler)
+harfler.append("a")
+harfler.remove("a")
+print(harfler )
+
+sayiler.sort()
+print(sayiler)
---
+++ tuple.py
@@ -0,0 +1,20 @@
+liste =  [1, 2, 3]
+
+tuple = ("bir", "iki","üç")
+
+print(liste)
+print(type(liste))
+print(tuple)
+print(type(tuple))
+
+print(tuple)
+
+
+liste[0]=7
+
+print(liste)
+
+#tuple[0]=yedi
+
+print(tuple.index("iki"))
+
