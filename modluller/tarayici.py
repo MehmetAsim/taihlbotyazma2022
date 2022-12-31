5119aacda177fbe1ff27741d7f80282a2b5dfcb5
MehmetAsim <mehmetasimkus@gmail.com>
2022-12-31 11:02:28 +0300

    Create tarayici.py

---
+++ modluller/tarayici.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+
+
+class Tarayici:
+    def__init__(self)
+        self.tarayici = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+    def al(self)
+        return self.tarayici
